7264726eb2c9c9350e2dbd2e8d610da1a6bf9f8e
emarino-git <marinomatias@outlook.com.ar>
2023-08-01 21:50:24 -0300

    implementing tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*{html,js}'],
+  content: [],
+  theme: {
+    colors: {
+      ...colors,
+    },
+  },
+};
+
